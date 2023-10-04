cf8df54c98ee64a49a22fbd519afc6757aef4684
Shahariar Sohan <Sohanshahariar4@gmail.com>
2023-10-04 17:13:24 +0600

    Loading state problem solved

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -10,23 +10,28 @@ import {
 import { auth } from "../Firebase/firebase.config";
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
   const createUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
   const Login = (email, password) => {
+    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
   const LogOut = () => {
+    setLoading(true);
     return signOut(auth);
   };
   useEffect(() => {
     const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
       console.log(currentUser);
       setUser(currentUser);
+      setLoading(false);
     });
     return () => unSubscribe();
   }, []);
-  const authInfo = { user, createUser, Login, LogOut };
+  const authInfo = { user, createUser, Login, LogOut, loading };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
--- src/Routes/PrivateRoot.jsx
+++ src/Routes/PrivateRoot.jsx
@@ -4,7 +4,21 @@ import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 
 const PrivateRoot = ({ children }) => {
-  const { user } = useContext(AuthContext);
+  const { user, loading } = useContext(AuthContext);
+  if (loading) {
+    return (
+      <div className="flex justify-center">
+        <span className="loading loading-spinner text-primary"></span>
+        <span className="loading loading-spinner text-secondary"></span>
+        <span className="loading loading-spinner text-accent"></span>
+        <span className="loading loading-spinner text-neutral"></span>
+        <span className="loading loading-spinner text-info"></span>
+        <span className="loading loading-spinner text-success"></span>
+        <span className="loading loading-spinner text-warning"></span>
+        <span className="loading loading-spinner text-error"></span>
+      </div>
+    );
+  }
   if (user) {
     return children;
   }
